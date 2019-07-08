a3921ffcd82f885bfcb743c9020a1dbac0f9f4e0
Goran Radmanovic <goranradmanovic@gmail.com>
2019-07-08 23:25:45 +0200

    Including new components and changing layout of the page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app.grid
+    header(class='grid__header')
+      Search
+
+    aside(class='grid__aside')
+      Aside
+
+    section(class='grid__content')
+      //-router-view
+      Table
+
+    footer(class='grid__footer')
+      Footer
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import Search from '@/components/Search.vue';
+  import Aside from '@/components/Aside.vue';
+  import Table from '@/components/Table.vue';
+  import Footer from '@/components/Footer.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      Search, Aside, Table, Footer
     }
   }
-}
+</script>
+
+<style lang="sass">
+  #app
 </style>
