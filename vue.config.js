95bd26d27c29e8b18d03389f4ed187ae642e47e1
Goran Radmanovic <goranradmanovic@gmail.com>
2019-07-15 07:42:02 +0200

    Adding comment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/TableApp/' : '/',
+  //publicPath: process.env.NODE_ENV === 'production' ? '/TableApp/' : '/',
 
   css: {
     loaderOptions: {
