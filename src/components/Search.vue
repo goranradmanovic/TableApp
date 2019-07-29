e9c4c1d948402378d857d4dae9884acebf6bced8
Goran Radmanovic <goranradmanovic@gmail.com>
2019-07-29 22:37:58 +0200

    Importing bus event and seting up search input field

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,23 @@
 <template lang="pug">
   form(action='' method='' autcomplete='off' class='grid__header--form')
     i.grid__header--form--icon.fas.fa-search
-    input(type='search' name='searchsomething' placeholder='Search by keywords, user, date or action' class='grid__header--form--search')
+    input(type='search' name='searchsomething' @input='searchBy()' v-model='query' placeholder='Search by tag items, assigned to or created date' class='grid__header--form--search')
 </template>
 
 <script>
+  import { bus } from '../main'
+
   export default {
-    name: 'Search'
+    name: 'Search',
+    data() {
+      return {
+        query: null
+      }
+    },
+    methods: {
+      searchBy() {
+        bus.$emit('search', this.query);
+      }
+    },
   }
 </script>
