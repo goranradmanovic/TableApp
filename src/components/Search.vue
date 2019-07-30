e6dee201d9326a2c0666d497d8e618f8491e4667
Goran Radmanovic <goranradmanovic@gmail.com>
2019-07-30 08:55:16 +0200

    Fixing some search bug

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   form(action='' method='' autcomplete='off' class='grid__header--form')
     i.grid__header--form--icon.fas.fa-search
-    input(type='search' name='searchsomething' @input='searchBy()' v-model='query' placeholder='Search by tag items, assigned to or created date' class='grid__header--form--search')
+    input(type='search' name='searchsomething' @input='searchBy()' v-model='query' placeholder='Search by tag items, assigned to or created date' title='Enter two or more characters' class='grid__header--form--search')
 </template>
 
 <script>
@@ -16,7 +16,11 @@
     },
     methods: {
       searchBy() {
-        bus.$emit('search', this.query);
+        if (this.query.length >= 2) {
+          bus.$emit('search', this.query);
+        } else if (this.query.length == 0) {
+          bus.$emit('search', this.query);
+        }
       }
     },
   }
